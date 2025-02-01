beedac724b1a820e3cbd3edc1af73837edb82158
ERICK ORTIZ VELÁSQUEZ <erick.ortiz.program@gmail.com>
2025-02-01 02:07:09 -0300

    Update dashboard.component.ts

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Chart, ChartType } from 'chart.js/auto';
+import { DashboardService } from 'src/app/services/dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,9 @@ import { Chart, ChartType } from 'chart.js/auto';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  year: any = {};
-  yearSelected: string = '2024';
-  yearsList: string[] = [];
+  data!: any;
+  yearSelected!: any;
+  periodList: any[] = [];
   chartIngresos!: Chart;
   chartGastos!: Chart;
   chartTotales!: Chart;
@@ -22,91 +23,161 @@ export class DashboardComponent implements OnInit {
   dataBarTotales!: any;
   optionsLine!: any;
   optionsBar!: any;
+  init: boolean = false;
   
-  data = [
-    {
-      year: '2022',
-      months: [
-        { shortName: 'Dic', name: "Diciembre", totalIncome: 1650000, essentialExpenses: 580000, nonEssentialExpenses: 520000 },
-        { shortName: 'Ene', name: "Enero", totalIncome: 900000, essentialExpenses: 400000, nonEssentialExpenses: 300000 },
-        { shortName: 'Feb', name: "Febrero", totalIncome: 950000, essentialExpenses: 420000, nonEssentialExpenses: 320000 },
-        { shortName: 'Mar', name: "Marzo", totalIncome: 1000000, essentialExpenses: 430000, nonEssentialExpenses: 350000 },
-        { shortName: 'Abr', name: "Abril", totalIncome: 1050000, essentialExpenses: 450000, nonEssentialExpenses: 360000 },
-        { shortName: 'May', name: "Mayo", totalIncome: 1100000, essentialExpenses: 470000, nonEssentialExpenses: 380000 },
-        { shortName: 'Jun', name: "Junio", totalIncome: 1150000, essentialExpenses: 480000, nonEssentialExpenses: 400000 },
-        { shortName: 'Jul', name: "Julio", totalIncome: 1200000, essentialExpenses: 500000, nonEssentialExpenses: 420000 },
-        { shortName: 'Ago', name: "Agosto", totalIncome: 1250000, essentialExpenses: 520000, nonEssentialExpenses: 440000 },
-        { shortName: 'Sep', name: "Septiembre", totalIncome: 1300000, essentialExpenses: 530000, nonEssentialExpenses: 450000 },
-        { shortName: 'Oct', name: "Octubre", totalIncome: 1350000, essentialExpenses: 540000, nonEssentialExpenses: 470000 },
-        { shortName: 'Nov', name: "Noviembre", totalIncome: 1400000, essentialExpenses: 550000, nonEssentialExpenses: 480000 },
-        { shortName: 'Dic', name: "Diciembre", totalIncome: 1500000, essentialExpenses: 560000, nonEssentialExpenses: 500000 }
-      ]
-    },
-    {
-      year: '2023',
-      months: [
-        { shortName: 'Dic', name: "Diciembre", totalIncome: 1500000, essentialExpenses: 560000, nonEssentialExpenses: 500000 },
-        { shortName: 'Ene', name: "Enero", totalIncome: 1100000, essentialExpenses: 450000, nonEssentialExpenses: 370000 },
-        { shortName: 'Feb', name: "Febrero", totalIncome: 1150000, essentialExpenses: 470000, nonEssentialExpenses: 380000 },
-        { shortName: 'Mar', name: "Marzo", totalIncome: 1200000, essentialExpenses: 480000, nonEssentialExpenses: 400000 },
-        { shortName: 'Abr', name: "Abril", totalIncome: 1250000, essentialExpenses: 490000, nonEssentialExpenses: 410000 },
-        { shortName: 'May', name: "Mayo", totalIncome: 1300000, essentialExpenses: 500000, nonEssentialExpenses: 420000 },
-        { shortName: 'Jun', name: "Junio", totalIncome: 1350000, essentialExpenses: 510000, nonEssentialExpenses: 430000 },
-        { shortName: 'Jul', name: "Julio", totalIncome: 1400000, essentialExpenses: 520000, nonEssentialExpenses: 450000 },
-        { shortName: 'Ago', name: "Agosto", totalIncome: 1450000, essentialExpenses: 530000, nonEssentialExpenses: 460000 },
-        { shortName: 'Sep', name: "Septiembre", totalIncome: 1500000, essentialExpenses: 540000, nonEssentialExpenses: 470000 },
-        { shortName: 'Oct', name: "Octubre", totalIncome: 1550000, essentialExpenses: 550000, nonEssentialExpenses: 480000 },
-        { shortName: 'Nov', name: "Noviembre", totalIncome: 1600000, essentialExpenses: 560000, nonEssentialExpenses: 500000 },
-        { shortName: 'Dic', name: "Diciembre", totalIncome: 1650000, essentialExpenses: 580000, nonEssentialExpenses: 520000 }
-      ]
-    },
-    {
-      year: '2024',
-      months: [
-        { shortName: 'Dic', name: "Diciembre", totalIncome: 1650000, essentialExpenses: 580000, nonEssentialExpenses: 520000 },
-        { shortName: 'Ene', name: "Enero", totalIncome: 1000000, essentialExpenses: 450000, nonEssentialExpenses: 350000 },
-        { shortName: 'Feb', name: "Febrero", totalIncome: 1000000, essentialExpenses: 450000, nonEssentialExpenses: 450000 },
-        { shortName: 'Mar', name: "Marzo", totalIncome: 1200000, essentialExpenses: 400000, nonEssentialExpenses: 450000 },
-        { shortName: 'Abr', name: "Abril", totalIncome: 1300000, essentialExpenses: 390000, nonEssentialExpenses: 500000 },
-        { shortName: 'May', name: "Mayo", totalIncome: 1150000, essentialExpenses: 350000, nonEssentialExpenses: 520000 },
-        { shortName: 'Jun', name: "Junio", totalIncome: 1350000, essentialExpenses: 360000, nonEssentialExpenses: 500000 },
-        { shortName: 'Jul', name: "Julio", totalIncome: 1100000, essentialExpenses: 480000, nonEssentialExpenses: 400000 },
-        { shortName: 'Ago', name: "Agosto", totalIncome: 1350000, essentialExpenses: 390000, nonEssentialExpenses: 500000 },
-        { shortName: 'Sep', name: "Septiembre", totalIncome: 1100000, essentialExpenses: 370000, nonEssentialExpenses: 500000 },
-        { shortName: 'Oct', name: "Octubre", totalIncome: 1300000, essentialExpenses: 250000, nonEssentialExpenses: 600000 },
-        { shortName: 'Nov', name: "Noviembre", totalIncome: 1000000, essentialExpenses: 320000, nonEssentialExpenses: 500000 },
-        { shortName: 'Dic', name: "Diciembre", totalIncome: 1200000, essentialExpenses: 560000, nonEssentialExpenses: 300000 }
-      ]
-    },
-    {
-      year: '2025',
-      months: [
-        { shortName: 'Dic', name: "Diciembre", totalIncome: 1200000, essentialExpenses: 560000, nonEssentialExpenses: 300000 },
-        { shortName: 'Ene', name: "Enero", totalIncome: 1000000, essentialExpenses: 450000, nonEssentialExpenses: 350000 },
-        { shortName: 'Feb', name: "Febrero", totalIncome: 1200000, essentialExpenses: 400000, nonEssentialExpenses: 300000 }
-      ]
-    }
-  ];
   
-  constructor() {}
+  constructor(private readonly _dashboardService: DashboardService) {}
 
   ngOnInit(): void {
-    const currentYear = new Date().getFullYear();
-    this.yearSelected = this.data.some((year: any) => year.year == currentYear) 
-      ? String(currentYear)
-      : String(currentYear - 1);
+    this.getAvailablePeriods();
+  }
 
-    this.year = this.data.find(year => year.year === this.yearSelected)!;
-    this.yearsList = this.data
-      .map(year => year.year)
-      .filter(year => year !== this.yearSelected);
+  getAvailablePeriods() {
+    this._dashboardService.getAvailablePeriods().subscribe({
+      next: async (response) => {
+        this.periodList = response;
+        this.yearSelected = this.periodList[0];
+        await this.getDataDashboard();
+        this.init = true;
+        console.log(response);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
 
-    this.dataLineIngresos = {
-      labels: this.year.months.slice(1).map((month: any) => month.shortName),
+  getDataDashboard() {
+    return new Promise<void>((resolve, reject) => {
+      this._dashboardService.getDataDashboard(this.yearSelected).subscribe({
+        next: (response: any) => {
+          this.data = response;
+          this.calculateAnnualValues();
+
+          if (this.init) {
+            this.updateCharts();
+          } else {
+            this.initCharts();
+          }
+          console.log(response);
+          resolve();
+        },
+        error: (error: any) => {
+          console.log('error:', error);
+          reject(error);
+        }
+      });
+    });
+  }
+
+  getArrowIcon(current: number, previous: number | null): string {
+    if (previous === null) {
+      return 'assets/img/flecha-no.svg';
+    }
+    if (current > previous) {
+      return 'assets/img/flecha-up.svg';
+    } else if (current < previous) {
+      return 'assets/img/flecha-down.svg';
+    } else {
+      return 'assets/img/flecha-no.svg';
+    }
+  }
+  
+  onYearChange(period: any) {
+    this.yearSelected = period;
+    this.getDataDashboard();
+  }
+  
+  updateCharts() {
+    const data1 = {
+      labels: this.data?.months.slice(1).map((month: any) => month?.month?.slice(0, 3)),
       datasets: [
         {
           label: 'Ingresos',
-          data: this.year.months.slice(1).map((month: any) => month.totalIncome),
+          data: this.data?.months.slice(1).map((month: any) => month.totalIncomes),
+          backgroundColor: '#00E676',
+          borderColor: '#00E676',
+          borderWidth: 1
+        }
+      ]
+    };
+  
+    const data2 = {
+      labels: this.data?.months.slice(1).map((month: any) => month?.month?.slice(0, 3)),
+      datasets: [
+        {
+          label: 'Gastos',
+          data: this.data?.months.slice(1).map((month: any) => month.totalEssentialExpenses + month.totalNotEssentialExpenses),
+          backgroundColor: '#FF1744',
+          borderColor: '#FF1744',
+          borderWidth: 1
+        }
+      ]
+    };
+  
+    const data3 = {
+      labels: this.data?.months.slice(1).map((month: any) => month?.month?.slice(0, 3)),
+      datasets: [
+        {
+          label: 'Ingresos',
+          data: this.data?.months.slice(1).map((month: any) => month.totalIncomes),
+          backgroundColor: '#00E676',
+          borderColor: '#00E676',
+          borderWidth: 1
+        },
+        {
+          label: 'Gastos Esenciales',
+          data: this.data?.months.slice(1).map((month: any) => month.totalEssentialExpenses),
+          backgroundColor: '#FF1744',
+          borderColor: '#FF1744',
+          borderWidth: 1
+        },
+        {
+          label: 'Gastos No Esenciales',
+          data: this.data?.months.slice(1).map((month: any) => month.totalNotEssentialExpenses),
+          backgroundColor: '#FFD700',
+          borderColor: '#FFD700',
+          borderWidth: 1
+        }
+      ]
+    };
+  
+    this.chartIngresos.data = data1;
+    this.chartIngresos.update();
+  
+    this.chartGastos.data = data2;
+    this.chartGastos.update();
+  
+    this.chartTotales.data = data3;
+    this.chartTotales.update();
+  }
+
+  calculateAnnualValues() {
+    const totalIncomes = this.data?.months.reduce((acc: any, month: any) => acc + month.totalIncomes, 0);
+    const totalEssentialExpenses = this.data?.months.reduce((acc: any, month: any) => acc + month.totalEssentialExpenses, 0);
+    const totalNotEssentialExpenses = this.data?.months.reduce((acc: any, month: any) => acc + month.totalNotEssentialExpenses, 0);
+    const totalRemaining = totalIncomes - (totalEssentialExpenses + totalNotEssentialExpenses);
+  
+    this.valueAnualTotalIngresos = totalIncomes;
+    this.valueAnualTotalGastosE = totalEssentialExpenses;
+    this.valueAnualTotalGastosNoE = totalNotEssentialExpenses;
+    this.valueAnualTotalRestante = totalRemaining;
+  
+    console.log('Totales anuales calculados:', {
+      ingresos: this.valueAnualTotalIngresos,
+      gastosE: this.valueAnualTotalGastosE,
+      gastosNoE: this.valueAnualTotalGastosNoE,
+      restante: this.valueAnualTotalRestante
+    });
+  }
+
+  initCharts() {
+    this.dataLineIngresos = {
+      labels: this.data?.months.slice(1).map((month: any) => month?.month?.slice(0, 3)),
+      datasets: [
+        {
+          label: 'Ingresos',
+          data: this.data?.months.slice(1).map((month: any) => month.totalIncomes),
           backgroundColor: '#00E676',
           borderColor: '#00E676',
           borderWidth: 1
@@ -115,11 +186,11 @@ export class DashboardComponent implements OnInit {
     };
     
     this.dataLineGastos = {
-      labels: this.year.months.slice(1).map((month: any) => month.shortName),
+      labels: this.data?.months.slice(1).map((month: any) => month?.month?.slice(0, 3)),
       datasets: [
         {
           label: 'Gastos',
-          data: this.year.months.slice(1).map((month: any) => month.essentialExpenses + month.nonEssentialExpenses),
+          data: this.data?.months.slice(1).map((month: any) => month.totalEssentialExpenses + month.totalNotEssentialExpenses),
           backgroundColor: '#FF1744',
           borderColor: '#FF1744',
           borderWidth: 1
@@ -128,25 +199,25 @@ export class DashboardComponent implements OnInit {
     };
 
     this.dataBarTotales = {
-      labels: this.year.months.map((month: any) => month.shortName),
+      labels: this.data?.months.slice(1).map((month: any) => month?.month?.slice(0, 3)),
       datasets: [
         {
           label: 'Ingresos',
-          data: this.year.months.map((month: any) => month.totalIncome),
+          data: this.data?.months.slice(1).map((month: any) => month.totalIncomes),
           backgroundColor: '#00E676',
           borderColor: '#00E676',
           borderWidth: 1
         },
         {
           label: 'Gastos Esenciales',
-          data: this.year.months.map((month: any) => month.essentialExpenses),
+          data: this.data?.months.slice(1).map((month: any) => month.totalEssentialExpenses),
           backgroundColor: '#FF1744',
           borderColor: '#FF1744',
           borderWidth: 1
         },
         {
           label: 'Gastos No Esenciales',
-          data: this.year.months.map((month: any) => month.nonEssentialExpenses),
+          data: this.data?.months.slice(1).map((month: any) => month.totalNotEssentialExpenses),
           backgroundColor: '#FFD700',
           borderColor: '#FFD700',
           borderWidth: 1
@@ -237,117 +308,5 @@ export class DashboardComponent implements OnInit {
       data: this.dataBarTotales,
       options: this.optionsBar
     });
-
-    this.calculateAnnualValues();
-  } 
-
-  getArrowIcon(current: number, previous: number | null): string {
-    if (previous === null) {
-      return 'assets/img/flecha-no.svg';
-    }
-    if (current > previous) {
-      return 'assets/img/flecha-up.svg';
-    } else if (current < previous) {
-      return 'assets/img/flecha-down.svg';
-    } else {
-      return 'assets/img/flecha-no.svg';
-    }
   }
-  
-  onYearChange(year: any) {
-    if (this.yearSelected !== undefined) {
-      this.yearsList = [...this.yearsList, this.yearSelected];
-      this.yearsList.sort((a: any, b: any) => a - b);
-    }
-    this.yearSelected = year;
-    this.year = this.data.find(y => y.year === this.yearSelected)!;
-    this.yearsList = this.yearsList.filter(y => y !== this.yearSelected);
-    this.updateCharts();
-    this.calculateAnnualValues();
-  }
-  
-  updateCharts() {
-    const data1 = {
-      labels: this.year.months.slice(1).map((month: any) => month.name),
-      datasets: [
-        {
-          label: 'Ingresos',
-          data: this.year.months.slice(1).map((month: any) => month.totalIncome),
-          backgroundColor: '#00E676',
-          borderColor: '#00E676',
-          borderWidth: 1
-        }
-      ]
-    };
-  
-    const data2 = {
-      labels: this.year.months.slice(1).map((month: any) => month.name),
-      datasets: [
-        {
-          label: 'Gastos',
-          data: this.year.months.slice(1).map((month: any) => month.essentialExpenses + month.nonEssentialExpenses),
-          backgroundColor: '#FF1744',
-          borderColor: '#FF1744',
-          borderWidth: 1
-        }
-      ]
-    };
-  
-    const data3 = {
-      labels: this.year.months.slice(1).map((month: any) => month.name),
-      datasets: [
-        {
-          label: 'Ingresos',
-          data: this.year.months.slice(1).map((month: any) => month.totalIncome),
-          backgroundColor: '#00E676',
-          borderColor: '#00E676',
-          borderWidth: 1
-        },
-        {
-          label: 'Gastos Esenciales',
-          data: this.year.months.slice(1).map((month: any) => month.essentialExpenses),
-          backgroundColor: '#FF1744',
-          borderColor: '#FF1744',
-          borderWidth: 1
-        },
-        {
-          label: 'Gastos No Esenciales',
-          data: this.year.months.slice(1).map((month: any) => month.nonEssentialExpenses),
-          backgroundColor: '#FFD700',
-          borderColor: '#FFD700',
-          borderWidth: 1
-        }
-      ]
-    };
-  
-    this.chartIngresos.data = data1;
-    this.chartIngresos.update();
-  
-    this.chartGastos.data = data2;
-    this.chartGastos.update();
-  
-    this.chartTotales.data = data3;
-    this.chartTotales.update();
-  }
-
-  calculateAnnualValues() {
-    const totalIncome = this.year.months.reduce((acc: any, month: any) => acc + month.totalIncome, 0);
-    const totalEssentialExpenses = this.year.months.reduce((acc: any, month: any) => acc + month.essentialExpenses, 0);
-    const totalNonEssentialExpenses = this.year.months.reduce((acc: any, month: any) => acc + month.nonEssentialExpenses, 0);
-    const totalRemaining = totalIncome - (totalEssentialExpenses + totalNonEssentialExpenses);
-  
-    this.valueAnualTotalIngresos = totalIncome;
-    this.valueAnualTotalGastosE = totalEssentialExpenses;
-    this.valueAnualTotalGastosNoE = totalNonEssentialExpenses;
-    this.valueAnualTotalRestante = totalRemaining;
-  
-    console.log('Totales anuales calculados:', {
-      ingresos: this.valueAnualTotalIngresos,
-      gastosE: this.valueAnualTotalGastosE,
-      gastosNoE: this.valueAnualTotalGastosNoE,
-      restante: this.valueAnualTotalRestante
-    });
-  }
-  
 }
-  
